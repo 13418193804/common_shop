7addbe77c3a23d6fc5feb772ed418190eea43d65
LiuLongTian-MAC <1750625203@qq.com>
2020-05-06 01:39:09 +0800

    no message

--- pages/home/index.js
+++ pages/home/index.js
@@ -267,14 +267,6 @@ Page({
           }
           this.getOilCardList()
 
-          app.reverseGeocoder((e) => {
-            console.log('home--', e)
-            this.setData({
-              city: e.address_component.city,
-              regionName: e.address_component.province,
-            })
-            this.getNetWorkList()
-          })
         },
         getNetWorkList() {
           api.get("/network/list", {
